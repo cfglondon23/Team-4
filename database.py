2d38a84ff82395ea86025f55e50b3869dadf81ef
hettysymes <hettysymes@users.noreply.github.com>
2023-04-06 13:28:13 +0100

    added functions to read from db

--- database.py
+++ database.py
@@ -7,14 +7,19 @@ DB_URL = 'https://volunteerapp-1dc3a-default-rtdb.firebaseio.com/'
 cred_obj = firebase_admin.credentials.Certificate(KEY_PATH)
 default_app = firebase_admin.initialize_app(cred_obj, {'databaseURL': DB_URL})
 
-
-
-# takes parameters, stores it to the database
+# takes parameters, stores it to the database, returns user id
 def createUserProfile(username, password, age):
     file_contents = {"username": username, "password": password, "age": age}
     ref = db.reference("/")
-    ref.child("users").push(file_contents)
+    return ref.child("users").push(file_contents).key
+
+# given user id, returns user info
+def getUserProfile(id):
+    ref = db.reference("/")
+    return ref.child("users").child(id).get()
 
 if __name__ == "__main__":
-    createUserProfile("bobsmith", "", 30)
-    createUserProfile("lilycarter", "", 22)
+    k1 = createUserProfile("bobsmith", "", 30)
+    k2 = createUserProfile("lilycarter", "", 22)
+    k3 = createUserProfile("jaycarter", "", 39)
+    print(getUserProfile(k3))
